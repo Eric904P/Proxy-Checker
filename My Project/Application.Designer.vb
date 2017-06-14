6422d660b1eaa159fa5b947781e3d6e581e9c827
Lewis Curl <bookworm904@gmail.com>
2017-06-14 00:51:57 -0400

    64-bit Release
    Out of memory errors with large proxylists

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Freelancer_Tutorial_Proxy_Checker.Form1
+            Me.MainForm = Global.Proxy_Checker.Form1
         End Sub
     End Class
 End Namespace
