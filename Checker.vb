a2787780ba0d099af2141fddc184f73ea633f7b6
Lewis Curl <bookworm904@gmail.com>
2017-08-01 04:38:58 -0400

    Revamp of checking process Cleaned up the code to reduce memory load ? Lots of changes

---
+++ Checker.vb
@@ -0,0 +1,121 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Threading
+
+Public Class Checker
+    '///////////////START OF CHECKER BLOCK/////////////////
+    Dim _thrdCnt As Integer = 0
+    Private _thrdMax As Integer = 50
+    ReadOnly _listLock As Object = New Object
+    ReadOnly _d As New Dictionary(Of String, Thread)()
+    ReadOnly _working As List(Of String) = New List(Of String)
+    Private _toCheck As List(Of String) = New List(Of String)
+    Private _toCheckTotal As Integer = 0
+    Public IsRunning As Boolean = False
+
+    Public Sub New(toCheck As List(Of String), thrdMax As Integer)
+        _toCheck = toCheck
+        _thrdMax = thrdMax
+    End Sub
+
+    Function CheckHerder() As Boolean
+        IsRunning = True
+        _toCheckTotal = _toCheck.Count
+        Dim thrdIndex = 1
+        While _toCheck.Count > 0 And IsRunning
+            If _thrdCnt <= _thrdMax Then
+                _d(thrdIndex.ToString) = New Thread(AddressOf CheckTask)
+                _d(thrdIndex.ToString).IsBackground = True
+                _d(thrdIndex.ToString).Start()
+                _thrdCnt = _thrdCnt + 1
+                thrdIndex = thrdIndex + 1
+            End If
+        End While
+        IsRunning = False
+        Return True
+    End Function
+
+    Private Sub CheckTask()
+        If _toCheck.Count > 0 Then
+            Dim toCheck As String
+            SyncLock _listLock
+                toCheck = _toCheck.Item(0)
+                _toCheck.RemoveAt(0)
+            End SyncLock
+            If CheckProxy(toCheck) Then
+                _working.Add(toCheck)
+                Console.WriteLine(toCheck)
+            End If
+            _thrdCnt = _thrdCnt - 1
+        End If
+    End Sub
+
+    'test single proxy
+    Function CheckProxy(proxy As String) As Boolean
+        Try 'uses azenv.net proxy judge
+            Dim r As HttpWebRequest = HttpWebRequest.Create("http://azenv.net")
+            r.UserAgent = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36"
+            r.Timeout = 3000
+            r.ReadWriteTimeout = 3000
+            r.Proxy = New WebProxy(proxy)
+            Using sr As New StreamReader(r.GetResponse().GetResponseStream())
+                If sr.ReadToEnd().Contains("HTTP_HOST = azenv.net") Then
+                    r.Abort()
+                    Return True
+                End If
+            End Using
+            r.Abort()
+        Catch ex As Exception
+            Return False
+        End Try
+        Return False
+    End Function
+
+    Public Function ReturnWorking() As List(Of String)
+        Return _working
+    End Function
+
+    Public Function ReturnThreadCount() As Integer
+        Return _thrdCnt
+    End Function
+
+    Public Function ReturnPercent() As Double
+        Return ((_toCheckTotal - _toCheck.Count) / _toCheckTotal)
+    End Function
+
+    Public Function ReturnCheckedCount() As Integer
+        Return (_toCheckTotal - _toCheck.Count)
+    End Function
+
+    Public Function ReturnToCheckTotal() As Integer
+        Return _toCheckTotal
+    End Function
+
+    Public Function GetThreadCount() As Integer
+        Return _thrdCnt
+    End Function
+
+    Public Function ReturnToCheckCount() As Integer
+        Return _toCheck.Count
+    End Function
+
+    Public Sub SetThreadMax(thrdMax As Integer)
+        _thrdMax = thrdMax
+    End Sub
+
+    Public Sub SetProxies(proxies As List(Of String))
+        _toCheck = proxies
+    End Sub
+
+    Public Sub Clear()
+        _toCheck.Clear()
+        _toCheckTotal = 0
+        _thrdCnt = 0
+        _working.Clear()
+        _d.Clear()
+    End Sub
+
+    Public Function ReturnWorkingCount() As Integer
+        Return _working.Count
+    End Function
+End Class
--- Form1.vb
+++ Form1.vb
@@ -3,27 +3,19 @@ Imports System.Net
 Imports System.Threading
 Public Class Form1
     'Dim l1, l2 As New ArrayList
-    Dim proxies, l1 As New List(Of String)
-    Dim l2 As Integer = 0
     Dim totalCount As Integer = 0
-    Dim thrdCnt As Integer = 0
-    Dim isBox As Boolean = False
     Dim isDone As Boolean = True
-    Dim d As New Dictionary(Of String, Thread)()
-    Private curProxLock As Object = New Object
-    Private l1Lock As Object = New Object
-    Private l2Lock As Object = New Object
-    Private indexLock As Object = New Object
-    Private uiCtrl As Thread = New Thread(AddressOf uiControler)
-    Private isRunning As Boolean = False
+    Private uiCtrl As Thread = New Thread(AddressOf UiControler)
+    Private Checker As Checker = New Checker(New List(Of String), TrackBar1.Value)
 
     'Load proxies dialogue 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If isRunning Then
+        Dim proxies As List(Of String) = New List(Of String)
+        If Checker.IsRunning Then
             MessageBox.Show("Please stop all threads first!")
             Exit Sub
         End If
-        clearVars()
+        Clear()
         Dim fo As New OpenFileDialog
         fo.RestoreDirectory = True
         fo.Multiselect = False
@@ -38,6 +30,7 @@ Public Class Form1
             End Using
         End If
         totalCount = proxies.Count()
+        Checker.SetProxies(proxies)
 
         ProgressBar1.Value = 0
         Dim percent As Integer = Math.Round((totalCount - proxies.Count()) / totalCount * 100)
@@ -46,9 +39,9 @@ Public Class Form1
     End Sub
 
     'zeroes variables used by program
-    Private Function clearVars() As Boolean
+    Private Function Clear() As Boolean
         Try
-            If d(1).IsAlive Then
+            If Checker.IsRunning Then
                 MessageBox.Show("Please stop all threads first.")
                 Return False
             End If
@@ -56,25 +49,11 @@ Public Class Form1
 
         End Try
 
-        proxies.Clear()
-        l1.Clear()
-        l2 = 0
+        'Program Reset
         totalCount = 0
-        d.Clear()
-        Return True
-    End Function
+        Checker.Clear()
 
-    'clears UI
-    Private Function clearFields() As Boolean
-        Try
-            If d(1).IsAlive Then
-                MessageBox.Show("Please stop all threads first.")
-                Return False
-            End If
-        Catch ex As Exception
-
-        End Try
-
+        'UI Reset
         Label1.Text = "Progress: 0.00% checked (0 left)"
         Label4.Text = "Unresponsive:"
         Label5.Text = "Working:"
@@ -84,16 +63,16 @@ Public Class Form1
         Label4.Update()
         Label5.Update()
         ProgressBar1.Update()
-
         Return True
     End Function
 
+
     'Stop button controller
-    Private Sub Button2_Click(sender As Object, e As EventArgs)
-        isRunning = False
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Checker.IsRunning = False
         Label1.Text = "Stopped!"
         Label1.Update()
-        While thrdCnt > 0
+        While Checker.GetThreadCount() > 0
             isDone = False
         End While
         isDone = True
@@ -101,11 +80,9 @@ Public Class Form1
 
     'Save dialogue
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        If (l1.Count() > 0) Then
+        If (Checker.ReturnWorking().Any()) Then
             Dim tempL As List(Of String)
-            SyncLock l1Lock
-                tempL = l1
-            End SyncLock
+            tempL = Checker.ReturnWorking()
             Dim fs As New SaveFileDialog
             fs.RestoreDirectory = True
             fs.Filter = "txt files (*.txt)|*.txt"
@@ -123,124 +100,39 @@ Public Class Form1
         End If
     End Sub
 
-    'Thread count controller
-    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
-        ToolTip1.SetToolTip(TrackBar1, TrackBar1.Value.ToString())
-        NumericUpDown1.Value = TrackBar1.Value
-        NumericUpDown1.Update()
-    End Sub
-
-    'test single proxy
-    Function checkProxy(proxy As String) As Boolean
-        Dim myProxy As WebProxy
-        Dim Temp As String
-        Try
-            myProxy = New WebProxy(proxy)
-            Dim r As HttpWebRequest = HttpWebRequest.Create("http://azenv.net")
-            r.UserAgent = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36"
-            r.Timeout = 3000
-            r.Proxy = myProxy
-            Dim re As HttpWebResponse = r.GetResponse()
-            Dim rs As Stream = re.GetResponseStream
-            Using sr As New StreamReader(rs)
-                Temp = sr.ReadToEnd()
-            End Using
-            Dim Text = Temp
-            rs.Dispose()
-            rs.Close()
-            r.Abort()
-            If Text.Contains("HTTP_HOST = azenv.net") Then
-                If Text.Contains("REQUEST_TIME =") Then
-                    Return True
-                End If
-            Else
-                Return False
-            End If
-        Catch ex As Exception
-
-        End Try
-        Return False
-    End Function
-
-    'thread action
-    Private Sub threadedProxyChecker()
-        Dim proxy As String
-        While isRunning
-            If proxies.Count() = 0 Then
-                Exit While
-            End If
-            SyncLock curProxLock
-                proxy = proxies.Item(0)
-                proxies.RemoveAt(0)
-            End SyncLock
-            If (checkProxy(proxy)) Then
-                l1.Add(proxy)
-            Else
-                l2 += 1
-            End If
-        End While
-        SyncLock l2Lock
-            thrdCnt = thrdCnt - 1
-        End SyncLock
-        'check for job completion
-        If thrdCnt = 0 And Not isBox Then
-            MessageBox.Show("Done checking!" & vbNewLine & l1.Count() & " working proxies")
-            isBox = True
-            isRunning = False
-            isDone = True
-        End If
-    End Sub
-
-    'second Stop button controller 
-    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
-        isRunning = False
-        Label1.Text = "Stopped!"
-        Label1.Update()
-        While thrdCnt > 0
-            isDone = False
-        End While
-        isDone = True
-    End Sub
-
     'Start button
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        If isRunning Then
+        If Checker.IsRunning Then
             MessageBox.Show("Program is already running!")
             Exit Sub
         End If
-        isBox = False
-        isRunning = True
         isDone = False
-        thrdCnt = TrackBar1.Value
+        Checker.SetThreadMax(TrackBar1.Value)
 
         uiCtrl.IsBackground = True
         uiCtrl.Start()
 
-        For int As Integer = 1 To thrdCnt Step 1
-            d(int.ToString) = New Thread(AddressOf threadedProxyChecker)
-            d(int.ToString).IsBackground = True
-            d(int.ToString).Start()
-        Next
+        If Checker.CheckHerder() Then
+            isDone = True
+        End If
     End Sub
 
     'Clear button
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-        If isRunning Then
+        If Checker.IsRunning Then
             MessageBox.Show("Please stop all threads first!")
             Exit Sub
         End If
-        If clearFields() Then
-            clearVars()
-        End If
+        Clear()
     End Sub
 
     'Copy to clipboard
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
         Dim clip As String = String.Empty
-        If l1.Count() > 0 Then
-            clip = String.Join(vbNewLine, l1.ToArray())
+        If Checker.ReturnWorking().Any() Then
+            clip = String.Join(vbNewLine, Checker.ReturnWorking().ToArray())
             Clipboard.SetText(clip)
-            MessageBox.Show(l1.Count() & " proxies copied to clipboard!")
+            MessageBox.Show(Checker.ReturnWorking().Count() & " proxies copied to clipboard!")
         Else
             MessageBox.Show("No proxies to copy!")
         End If
@@ -252,32 +144,30 @@ Public Class Form1
     End Sub
 
     'UI control thread
-    Private Sub uiControler()
-        Dim percent As Double = 0
+    Private Sub UiControler()
         While Not isDone
             Label5.Invoke(Sub()
-                              Label5.Text = "Working: " & l1.Count()
+                              Label5.Text = "Working: " & Checker.ReturnWorkingCount()
                               Label5.Update()
                           End Sub)
             Label4.Invoke(Sub()
-                              Label4.Text = "Unresponsive: " & l2
+                              Label4.Text = "Unresponsive: " & (Checker.ReturnCheckedCount() - Checker.ReturnWorkingCount())
                               Label4.Update()
                           End Sub)
-            percent = Math.Round(((totalCount - proxies.Count()) / totalCount * 100), 2, MidpointRounding.AwayFromZero)
             Label1.Invoke(Sub()
-                              Label1.Text = "Progress: " & percent & "% checked " & "(" & proxies.Count() & " left)"
+                              Label1.Text = "Progress: " & Math.Round(Checker.ReturnPercent(), 2, MidpointRounding.AwayFromZero) & "% checked " & "(" & Checker.ReturnToCheckCount() & " left)"
                               Label1.Update()
                           End Sub)
             ProgressBar1.Invoke(Sub()
-                                    ProgressBar1.Value = Math.Round(percent, 0)
+                                    ProgressBar1.Value = Math.Round(Checker.ReturnPercent(), 0)
                                     ProgressBar1.Update()
                                 End Sub)
             Label7.Invoke(Sub()
-                              Label7.Text = thrdCnt
+                              Label7.Text = Checker.GetThreadCount()
 
                               Label7.Update()
                           End Sub)
-            uiCtrl.Sleep(1)
+            Thread.Sleep(100)
             'Thread.Sleep(1)
         End While
     End Sub
